4023431d06e8e1bb6725499a81f87db19c6baff0
eespinetti <eespinetti@4geeks.com.ve>
2016-03-05 15:01:18 -0430

    Modificando vista de encuesta

--- public/knockout/view_models/evaluadores_view_model.js
+++ public/knockout/view_models/evaluadores_view_model.js
@@ -17,5 +17,10 @@ function evaluadoresViewModel(){
 		self.showForm(!self.showForm());
 	};
 
+	$.getJSON("http://localhost:8000/admin/users", function(data) {
+		console.log(data);
+		// Now use this data to update your view models,
+		// and Knockout will update your UI automatically
+	})
 
 }
--- resources/views/admin/add_user.blade.php
+++ resources/views/admin/add_user.blade.php
@@ -6,7 +6,7 @@
 		<div class="box box-primary">
 			<div class="box-header with-border">
 				<h3 class="box-title">
-					Evaluadores
+					Encuestados
 				</h3>
 			</div>
 			<div class="box-body">
