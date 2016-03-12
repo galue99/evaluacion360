82bcb52490ea73e980f6642da5b28770e56dc4ac
EricksonCarreno <erick2109@gmail.com>
2016-03-12 18:36:05 +0000

    terminando la recoleccion de los datos de la encuesta y metiendolos en unarray de objeto

--- public/knockout/view_models/test_view_model.js
+++ public/knockout/view_models/test_view_model.js
@@ -6,15 +6,14 @@ function testViewModel(){
 	self.items = ko.observableArray();
 	self.currentItem = ko.observable({frases: [{name: 'xx'}]});
 	self.currentIndexFrase = ko.observable(0);
-	
+
 	self.finish =  ko.observable(false);
-	
-	self.answersSelected = ko.observable();
-	self.allFrases = ko.observableArray();
-	self.formData = ko.observable({
-		answers: ko.observable()
-	});
-	
+  self.currentAnswer = ko.observable();
+
+  self.formData = ko.observable({
+    answers: ko.observableArray() //con este l
+  });
+
 
 
 	self.toggleEncuesta = function(){
@@ -23,16 +22,39 @@ function testViewModel(){
 
 
 	self.next = function() {
+     if (self.currentAnswer() == null) {
+        alert('seleccione una respuesta');
+        return;
+      }
+
+    var setAnswer =  function(){
+    //  self.currentIndexFrase(self.currentIndexFrase() + 1);
+      self.formData().answers.push({
+         frase_id: self.currentAnswer().frase_id,
+         answer_id: self.currentAnswer().idanswer
+
+      });
+
+      self.currentAnswer(null);
+    }
 		if (self.currentIndexFrase() == self.currentItem().frases.length  - 1 && !self.finish() ){
+      //console.log(self.items()[self.items.indexOf(self.currentItem()) + 1] );
+      setAnswer();
+
+
+
 			self.currentIndexFrase(0);
 			self.currentItem(self.items()[self.items.indexOf(self.currentItem()) + 1]);
-			self.finish( self.items.indexOf(self.currentItem()) == self.items().length - 1 );
-			
-		}else{
-			self.currentIndexFrase(self.currentIndexFrase() + 1);
 
-			console.log(self.currentItem().frases[self.currentIndexFrase() - 1].answers);
+			self.finish( self.items.indexOf(self.currentItem()) == self.items().length - 1 );
+
+
+		}else{
+			setAnswer();
+      self.currentIndexFrase(self.currentIndexFrase() + 1);
+
 		}
+    console.log(self.formData().answers());
 	}
 
 	self.findTest = function(){
@@ -40,8 +62,11 @@ function testViewModel(){
 		.done(function(response){
         self.items(response.items);
         self.currentItem(self.items()[0]);
+
 		});
 	};
+
+
 
 
 
