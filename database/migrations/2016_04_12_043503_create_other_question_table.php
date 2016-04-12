89a8962af894e14388e089068203259154df3280
EricksonCarreno <ecarreno@datawebonline.net>
2016-04-12 00:58:51 -0430

    subiendo migraciones para las tablas de otras preguntas y para la tabla de guardar las respuesta de otras preguntas

---
+++ database/migrations/2016_04_12_043503_create_other_question_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtherQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('others_questions', function(Blueprint $table){
+            $table->increments('id');
+            $table->text('question');
+            $table->integer('encuestas_id');
+            $table->foreing('encuestas_id')->references('id')->on('encuestas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('others_questions');
+    }
+}
